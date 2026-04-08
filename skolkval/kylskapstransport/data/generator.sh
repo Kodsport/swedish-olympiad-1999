18802fa9e841be7289d269ce23eedafd156acb10
Matistjati <matistjati@outlook.com>
2026-04-09 00:27:32 +0200

    Fixes

--- skolkval/kylskapstransport/data/generator.sh
+++ skolkval/kylskapstransport/data/generator.sh
@@ -5,7 +5,6 @@
 use_solution kylskap_js.cc
 
 samplegroup
-limits onlya=0
 sample 1
 
 group group1 33
@@ -13,7 +12,6 @@ limits onlya=1
 tc_manual t3
 
 group group2 67
-limits onlya=0
 include_group sample
 include_group group1
 tc_manual t1
--- skolkval/kylskapstransport/input_validators/validator/validator.cpp
+++ skolkval/kylskapstransport/input_validators/validator/validator.cpp
@@ -1,15 +1,20 @@
 #include "validator.h"
 
 void run() {
-    bool onlya = Arg("onlya");
+    bool onlya = Arg("onlya", false);
 
-    int pa = Int(500, 2000); Space();
-    int ka = Int(10, 50); Space();
-    int pb = Int(500, 2000); Space();
-    int kb = Int(10, 50); Space();
-    int n = Int(1, 1000); Endl();
+    int pa = Int(500, 2000);
+    Space();
+    int ka = Int(10, 50);
+    Space();
+    int pb = Int(500, 2000);
+    Space();
+    int kb = Int(10, 50);
+    Space();
+    int n = Int(1, 1000);
+    Endl();
 
-    int opt = pa * n;
+    int opt = INT_MAX;
     pair<int, int> sol;
     for (int i = 0; i <= 100; i++) {
         for (int j = 0; j <= 100; j++) {
@@ -20,6 +25,7 @@ void run() {
             }
         }
     }
+    assert(opt != INT_MAX);
 
     int num_sols = 0;
     for (int i = 0; i <= 100; i++) {
